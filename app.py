a6e27567add7945bdc50c5a4776adefdb94b1179
BOLT-TANKS <vvk.bolt@gmail.com>
2025-02-26 17:24:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -130,4 +130,4 @@ def index():
         try:
             requests.post(formbold_url, json=response_data)
         except Exception as formbold_error:
-            print(f"
+            print(f"FormBold error: {formbold_error}")
